8aace64d623c94b86a9060aeb9444cdf75cff830
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-09-24 21:07:11 -0500

    added more questions and a function

--- Develop/index.js
+++ Develop/index.js
@@ -3,32 +3,82 @@
 const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a brief description about your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe the installation process if any',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is this project used for?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: [
+                'Apache',
+                'Academic',
+                'GNU',
+                'ISC',
+                'MIT',
+                'Mozilla',
+                'Open',
+            ],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Who contributed to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Is there a test included?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What should i do if i have an issue?',
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your GitHub username: ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email: ',
+        },
+    ])
+}
 
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is your project title?',
-                name: 'Title',
-            },
-            {
-                type: 'input',
-                message: '',
-                name: '',
-            },
-            {
-                
-            }
-        ])
-
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('Readme successfully created')
+    } catch(err) {
+        console.log(err)
+    }
+}
 
 // Function call to initialize app
 init();
